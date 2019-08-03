3fee81ced2cd56793e2e2af1aac78f93fd9e526f
“wlhuang <wenlong.huang@ucchip.com>
2019-08-03 15:47:17 +0800

    Singleton Pattern Done!

--- Singleton/main.cpp
+++ Singleton/main.cpp
@@ -5,28 +5,49 @@ using namespace std;
 class LazySingleton //Lazy Pattern
 {
 private:
-	LazySingleton(){}
+	LazySingleton(){
+		cout<<"Construct lazy singleton Succeed!"<<endl;
+	}
 	static LazySingleton *m_pInstance;
 public:
 	static LazySingleton * GetInstance()
 	{
 		if(m_pInstance == NULL) {
 			m_pInstance = new LazySingleton();
-			cout<<"Construct lazy singleton Succeed!"<<endl;
 		}
 		else {
-			cout<<"The lazy singleton has already been constructed. No need to do again."<<endl;
+			cout<<"The lazy singleton has already been constructed. Get lazy singleton Succeed!"<<endl;
 		}
 		return m_pInstance;
 	}
 };
 
+class HungrySingleton //Hungry Pattern
+{
+private:
+	HungrySingleton(){
+		cout<<"Construct hungry singleton Succeed!"<<endl;
+	}
+	static HungrySingleton *m_hpInstance;
+public:
+	static HungrySingleton * GetInstance()
+	{
+		cout<<"Get hungry singleton Succeed!"<<endl;
+		return m_hpInstance;
+	}
+};
+
 LazySingleton* LazySingleton::m_pInstance = NULL;	//static variables need to be inited out of class.
+HungrySingleton* HungrySingleton::m_hpInstance = new HungrySingleton;
 
 int main()
 {
 	LazySingleton* p1 = LazySingleton::GetInstance();
 	LazySingleton* p2 = p1 -> GetInstance();
 	LazySingleton & ref = *LazySingleton::GetInstance();
+	
+	HungrySingleton* h1 = HungrySingleton::GetInstance();
+	HungrySingleton* h2 = h1 -> GetInstance();
+	HungrySingleton & href = *HungrySingleton::GetInstance();
 	return 0;
 }
